a7be20f726f9527c21f2da69a37f16766158448b
Tony1527 <dingxiaoqiyx@sina.com>
2016-12-11 23:46:38 +0800

    1.我新写了一些文件注释 2.添加了路径检查函数 ————Tony1527

--- DirTest.c
+++ DirTest.c
@@ -4,11 +4,12 @@
 #include<time.h>
 #include"dentry.h"
 #include"Queue_d_address.h"
-#define FILEPATH_ERROR -1
-#define OUT_OF_ADDRESS NULL
+#define FILEPATH_INVALID -2			//路径包含非法字符
+#define FILEPATH_ERROR -1			//路径出错，有文件/文件夹不存在的情况
+#define OUT_OF_ADDRESS NULL			//超出了dentry的范围
 #define parse(address) parseDArress(address)
 #define dentry_each_child(parent_address,pdentry_child,pdentry_lsibling) Pdentry __pf=parse(parent_address);d_address __childAddr=__pf->d_subdir;\
-for(pdentry_lsibling = parse(parent_address),pdentry_child = parse(__childAddr);__childAddr!=EOF;pdentry_lsibling = pdentry_child,__childAddr=pdentry_child->d_rsibling,pdentry_child = __childAddr!=EOF?parse(__childAddr):NULL)
+for(pdentry_lsibling = parse(parent_address),pdentry_child = parse(__childAddr);__childAddr!=EOF;pdentry_lsibling = pdentry_child,__childAddr=pdentry_child->d_rsibling,pdentry_child = __childAddr!=EOF?parse(__childAddr):NULL)	//这个宏的意思是将父节点的d_address读入，还有两个指针用来接收指向孩子的遍历指针pdentry_child，以及在初始化时指向父节点pdentry_lsibling，后来这个指针用来标识当前正在读入节点的d_address
 typedef struct{
 	char B[2*1024];
 }PhysicalBlock;
@@ -18,18 +19,19 @@ typedef struct{
 	int located;
 }PhysicalBlocks;
 
+
 critical_dir* my_dir,* normal_cd[6];
 PhysicalBlocks* vf;
 d_address current_address;
 
 void* getPhysicalBlock();
-void InitCDBlocks(critical_dir* cdir);
-Pdentry inline parseDArress(d_address dAddr);
+void InitCDBlocks(critical_dir* cdir);			//初始化新得到的cd块
+Pdentry inline parseDArress(d_address dAddr);		//解析dAddr偏移量所在的dentry,如果偏移量不存在或者超出磁盘空间，则返回NULL			
 d_address applyDentry();
 void _printSubdir(d_address dAddr,int depth);
 Pdentry _mkdir_d(d_address curAddr,char* filename);
 Pdentry _rmdir_d(d_address curAddr);
-void Initial();
+void Initial();					
 d_address _scan_d(char* filepath);
 
 
@@ -38,20 +40,20 @@ void InitCDBlocks(critical_dir* cdir){
 	int j;
 	for(j=0;j<MAX_AVAILABLE;j++){
                	memset(cdir->dentry_arr[j].d_filename,0,MAX_FILENAME*sizeof(char));
-               	memset(&(cdir->dentry_arr[j].d_subdir),EOF,4*sizeof(d_address));
-
+               	memset(&(cdir->dentry_arr[j].d_subdir),EOF,4*sizeof(d_address));	//把d_address设置成EOF，即对应没有相邻的节点。
         }
 	cdir->freeSpace =(d_bitmap*) malloc(sizeof(d_bitmap));
+	*(cdir->freeSpace) = 0x0;				//位示图初始化
         cdir->nextCD = EOF;
 }
 
 //解析dAddr偏移量所在的dentry,如果偏移量不存在或者超出磁盘空间，则返回NULL
 Pdentry inline parseDArress(d_address dAddr){
-	short bid =dAddr/MAX_AVAILABLE;
+	short bid =dAddr/MAX_AVAILABLE;			//获得dAddr所在CD块的块号
 	int i=0;
-	if(dAddr == FILEPATH_ERROR)
+	if(dAddr == FILEPATH_ERROR || dAddr == FILEPATH_INVALID)
 		return NULL;
-	for(;i<6;i++){
+	for(;i<6;i++){/*此处在吴大师写好之后需要修改，因为有可能不在系统内存中*/
 		if(normal_cd[i]->id==bid)
 			return &normal_cd[i]->dentry_arr[dAddr%MAX_AVAILABLE];
 	}
@@ -62,7 +64,7 @@ Pdentry inline parseDArress(d_address dAddr){
 d_address applyDentry(){
 	int i;
 	d_address location;
-	for(i=0;i<6;i++){
+	for(i=0;i<6;i++){/*此处在吴大师写好之后需要修改，因为有可能不在系统内存中*/
 		if(_addDentry(normal_cd[i]->freeSpace,&location,i)!=CD_BLOCK_FULL){
 			printf("location:%d\n",location);
 			return location;
@@ -76,21 +78,21 @@ void _printSubdir(d_address dAddr,int depth){
 	Pdentry pf = parse(dAddr);
 	int i;
 	d_address sAddr;
-	Queue_d_address* queue = malloc(sizeof(Queue_d_address));
+	Queue_d_address* queue = malloc(sizeof(Queue_d_address));	//队列用于打印树状结构
 	InitQueue(queue);
 	Pdentry ps,pls;
 	for(i=0;i<depth;i++)printf("\t");
-	printf("%s\n",pf->d_filename);
+	printf("filename:%s parent:%d rsibling:%d subdir:%d\n",pf->d_filename,pf->d_parent,pf->d_rsibling,pf->d_subdir);
 	dentry_each_child(dAddr,ps,pls){
 		for(i=0;i<depth+1;i++)printf("\t");
-		printf("%s\n",ps->d_filename);
-		if(pls == parse(dAddr))	
+		printf("filename:%s parent:%d rsibling:%d subdir:%d\n",ps->d_filename,ps->d_parent,ps->d_rsibling,ps->d_subdir);
+		if(pls == parse(dAddr))					//如果是父节点就把子目录压入	
 			Enqueue(queue,&(pls->d_subdir));
-		else
+		else							//如果已经是子节点了，就把其右兄弟压入
 			Enqueue(queue,&(pls->d_rsibling));
 	}
 	printf("%080d\n",0);
-	while(IsQueueEmpty(queue)==0){
+	while(IsQueueEmpty(queue)==0){					//递归解析
 		Dequeue(queue,&sAddr);
 		_printSubdir(sAddr,depth+1);
 	}
@@ -157,6 +159,20 @@ Pdentry _rmdir_d(d_address curAddr){
         memset(&(normal_cd[i]->dentry_arr[curAddr%MAX_AVAILABLE].d_subdir),EOF,4*sizeof(d_address));
 }
 
+
+int _checkFilepath(char* filepath){
+	char c;
+	int i;
+	for(i=0;i<strlen(filepath);i++){
+		c = filepath[i];
+		if((c>='-'&&c<=57) || (c>=65&&c<=90 || (c>=97&&c<=122)))
+			continue;
+		else
+			return FILEPATH_INVALID;
+	}
+	return 0;
+}
+
 //扫描路径，如果正确则返回对应d_address,否则返回不同类型错误
 d_address _scan_d(char* filepath){
 	char* _filepath = malloc((strlen(filepath)+1)*sizeof(char));
@@ -165,30 +181,34 @@ d_address _scan_d(char* filepath){
 	d_address dAddr;
 	Pdentry ps,pls;
 	strcpy(_filepath,filepath);
-	if(_filepath[0] == '/')
-		dAddr = 0;
-	else
-		dAddr = current_address;
-	_filename = strtok(_filepath,"/");
-	while(_filename!=NULL){
-		catch_flag = 0;
-		//printf("%s\n",_filename);
-		dentry_each_child(dAddr,ps,pls){
-			if(!strcmp(ps->d_filename,_filename)){
-				if(pls == parse(dAddr))
-					dAddr = pls->d_subdir;	
-				else{
-					dAddr = pls->d_rsibling;
+	if(_checkFilepath(_filepath)!=FILEPATH_INVALID){
+		if(_filepath[0] == '/')
+			dAddr = 0;
+		else
+			dAddr = current_address;
+		_filename = strtok(_filepath,"/");
+		while(_filename!=NULL){
+			catch_flag = 0;
+			//printf("%s\n",_filename);
+			dentry_each_child(dAddr,ps,pls){
+				if(!strcmp(ps->d_filename,_filename)){
+					if(pls == parse(dAddr))
+						dAddr = pls->d_subdir;	
+					else{
+						dAddr = pls->d_rsibling;
+					}
+					catch_flag = 1;
+					break;
 				}
-				catch_flag = 1;
-				break;
 			}
+			if(!catch_flag)		
+				return 	FILEPATH_ERROR;			
+			_filename = strtok(NULL,"/");		
 		}
-		if(!catch_flag)		
-			return 	FILEPATH_ERROR;			
-		_filename = strtok(NULL,"/");		
+		return dAddr;
+	}else{
+		return FILEPATH_INVALID;
 	}
-	return dAddr;
 }
 
 
@@ -214,8 +234,8 @@ void Initial(){
 	_mkdir_d(pf->d_subdir,"tony");
 	current_address = 0;
 //	_rmdir_d(1);
-	//_printSubdir(root,0);
-	printf("%s\n",parse(_scan_d("local/tony"))==NULL?"yes":"no");
+	_printSubdir(root,0);
+	printf("%s\n",_scan_d("local/tony.c") == FILEPATH_ERROR?"yes":"no");
 }
 
 int main(){
